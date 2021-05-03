4ba6054cdfa093816c3ef9312925d510e3a4d846
Charles Montaigne <charles.montaigne@gmail.com>
2021-05-03 20:52:12 +0200

    Domain

---
+++ BankAccountManager3000.Domain/Entities/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountManager3000.Domain.Entities
+{
+    public class BankAccount
+    {
+        public int Id { get; init; }
+        public string BankName { get; init; }
+        public string Iban { get; init; }
+        public string Bic { get; init; }
+        public string HolderName { get; init; }
+        public DateTime CreationDate { get; init; }
+    }
+}
