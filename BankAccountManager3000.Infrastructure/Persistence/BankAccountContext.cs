1d30ae981a505f1257243b5983d85b5352b8d185
Charles Montaigne <charles.montaigne@gmail.com>
2021-05-03 21:53:50 +0200

    Persistence

---
+++ BankAccountManager3000.Infrastructure/Persistence/BankAccountContext.cs
@@ -0,0 +1,38 @@
+﻿using BankAccountManager3000.Application.Common.Interfaces;
+using BankAccountManager3000.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BankAccountManager3000.Infrastructure
+{
+    public class BankAccountContext : DbContext, IBankAccountContext
+    {
+        public DbSet<BankAccount> BankAccounts { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BankAccount>(entity =>
+            {
+                entity.HasAlternateKey(x => x.Iban);
+            });
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Filename=../BankAccountData.sqlite");
+        }
+    }
+}
---
+++ BankAccountManager3000.Infrastructure/Persistence/Migrations/20210503195231_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankAccountManager3000.Infrastructure.Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BankName = table.Column<string>(type: "TEXT", nullable: true),
+                    Iban = table.Column<string>(type: "TEXT", nullable: false),
+                    Bic = table.Column<string>(type: "TEXT", nullable: true),
+                    HolderName = table.Column<string>(type: "TEXT", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.Id);
+                    table.UniqueConstraint("AK_BankAccounts_Iban", x => x.Iban);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+        }
+    }
+}
